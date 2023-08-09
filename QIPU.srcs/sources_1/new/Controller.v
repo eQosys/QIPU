60518747cb542cd6e532afab5a7600b3737d967e
TecStylos <tecstylos@gmail.com>
2023-08-09 07:53:17 +0200

    Update compile order when bootloader changes

--- QIPU.srcs/sources_1/new/Controller.v
+++ QIPU.srcs/sources_1/new/Controller.v
@@ -32,31 +32,38 @@ module Controller(
     localparam opcode_int = 5'b01011;
     
     localparam aluControl_unused = 3'bzzz;
-    localparam aluControl_add = 3'b000;
-    localparam aluControl_sub = 3'b001;
-    localparam aluControl_and = 3'b010;
-    localparam aluControl_or  = 3'b011;
-    localparam aluControl_xor = 3'b100;
+    localparam aluControl_add    = 3'b000;
+    localparam aluControl_sub    = 3'b001;
+    localparam aluControl_and    = 3'b010;
+    localparam aluControl_or     = 3'b011;
+    localparam aluControl_xor    = 3'b100;
     
-    localparam offsetLayout_unused = 2'bzz;
-    localparam offsetLayout_normal = 2'b00;
-    localparam offsetLayout_short = 2'b01;
+    localparam offsetLayout_unused         = 2'bzz;
+    localparam offsetLayout_normal         = 2'b00;
+    localparam offsetLayout_short          = 2'b01;
     localparam offsetLayout_longContinuous = 2'b10;
-    localparam offsetLayout_longSplit = 2'b11;
+    localparam offsetLayout_longSplit      = 2'b11;
     
     localparam immExtendMode_unused = 2'b00;
-    localparam immExtendMode_upper = 2'b01;
-    localparam immExtendMode_lower = 2'b10;
-    localparam immExtendMode_full = 2'b11;
+    localparam immExtendMode_upper  = 2'b01;
+    localparam immExtendMode_lower  = 2'b10;
+    localparam immExtendMode_full   = 2'b11;
     
     localparam resultSelect_unused = 2'bzz;
-    localparam resultSelect_alu = 2'b00;
-    localparam resultSelect_mem = 2'b01;
-    localparam resultSelect_imm = 2'b10;
+    localparam resultSelect_alu    = 2'b00;
+    localparam resultSelect_mem    = 2'b01;
+    localparam resultSelect_imm    = 2'b10;
     
     localparam aluSrcASelect_unused = 1'bz;
-    localparam aluSrcASelect_reg = 1'b0;
-    localparam aluSrcASelect_zero = 1'b1;
+    localparam aluSrcASelect_reg    = 1'b0;
+    localparam aluSrcASelect_zero   = 1'b1;
+    
+    localparam jmpCond_zero         = 4'b0000;
+    localparam jmpCond_notZero      = 4'b0001;
+    localparam jmpCond_greaterZero  = 4'b0010;
+    localparam jmpCond_lessZero     = 4'b0011;
+    localparam jmpCond_greaterEqual = 4'b0100;
+    localparam jmpCond_lessEqual    = 4'b0101;
     
     always @ (*) begin
         case (opcode_in)
@@ -113,12 +120,12 @@ module Controller(
             opcode_jxx: begin // jxx
                 aluControl_out <= aluControl_add;
                 case (jmpCond_in)
-                    4'b0000: doJump_out <= isZero_in; // zero
-                    4'b0001: doJump_out <= ~isZero_in; // not zero
-                    4'b0010: doJump_out <= ~isNeg_in & ~isZero_in; // greater than zero
-                    4'b0011: doJump_out <= isNeg_in; // less than zero
-                    4'b0100: doJump_out <= ~isNeg_in; // greater than or equal to zero
-                    4'b0101: doJump_out <= isNeg_in | isZero_in; // less than or equal to zero
+                    jmpCond_zero:         doJump_out <= isZero_in; // zero
+                    jmpCond_notZero:      doJump_out <= ~isZero_in; // not zero
+                    jmpCond_greaterZero:  doJump_out <= ~isNeg_in & ~isZero_in; // greater than zero
+                    jmpCond_lessZero:     doJump_out <= isNeg_in; // less than zero
+                    jmpCond_greaterEqual: doJump_out <= ~isNeg_in; // greater than or equal to zero
+                    jmpCond_lessEqual:    doJump_out <= isNeg_in | isZero_in; // less than or equal to zero
                     default: doJump_out <= 0;
                 endcase
                 regWriteEnable_out <= 0;
