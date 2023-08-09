9346c21a2a51f724d1c7903d218681fc5cd8ab3c
TecStylos <tecstylos@gmail.com>
2023-08-09 07:40:55 +0200

    Controller cleanup

--- QIPU.srcs/sources_1/new/Controller.v
+++ QIPU.srcs/sources_1/new/Controller.v
@@ -18,65 +18,100 @@ module Controller(
         output reg [1:0] immExtendMode_out
     );
     
+    localparam opcode_add = 5'b00000;
+    localparam opcode_sub = 5'b00001;
+    localparam opcode_and = 5'b00010;
+    localparam opcode_or  = 5'b00011;
+    localparam opcode_xor = 5'b00100;
+    localparam opcode_jxx = 5'b00101;
+    localparam opcode_st  = 5'b00110;
+    localparam opcode_ld  = 5'b00111;
+    localparam opcode_lui = 5'b01000;
+    localparam opcode_lli = 5'b01001;
+    localparam opcode_li  = 5'b01010;
+    localparam opcode_int = 5'b01011;
+    
+    localparam aluControl_unused = 3'bzzz;
+    localparam aluControl_add = 3'b000;
+    localparam aluControl_sub = 3'b001;
+    localparam aluControl_and = 3'b010;
+    localparam aluControl_or  = 3'b011;
+    localparam aluControl_xor = 3'b100;
+    
+    localparam offsetLayout_unused = 2'bzz;
     localparam offsetLayout_normal = 2'b00;
     localparam offsetLayout_short = 2'b01;
     localparam offsetLayout_longContinuous = 2'b10;
     localparam offsetLayout_longSplit = 2'b11;
     
+    localparam immExtendMode_unused = 2'b00;
+    localparam immExtendMode_upper = 2'b01;
+    localparam immExtendMode_lower = 2'b10;
+    localparam immExtendMode_full = 2'b11;
+    
+    localparam resultSelect_unused = 2'bzz;
+    localparam resultSelect_alu = 2'b00;
+    localparam resultSelect_mem = 2'b01;
+    localparam resultSelect_imm = 2'b10;
+    
+    localparam aluSrcASelect_unused = 1'bz;
+    localparam aluSrcASelect_reg = 1'b0;
+    localparam aluSrcASelect_zero = 1'b1;
+    
     always @ (*) begin
         case (opcode_in)
-            5'b00000: begin // add
-                aluControl_out <= 3'b000;
+            opcode_add: begin // add
+                aluControl_out <= aluControl_add;
                 doJump_out <= 0;
                 regWriteEnable_out <= 1;
                 memWriteEnable_out <= 0;
-                aluSrcASelect_out <= 0;
+                aluSrcASelect_out <= aluSrcASelect_reg;
                 offsetLayout_out <= offsetLayout_normal;
-                resultSelect_out <= 0;
-                immExtendMode_out <= 0;
+                resultSelect_out <= resultSelect_alu;
+                immExtendMode_out <= immExtendMode_unused;
                 end
-            5'b00001: begin // sub
-                aluControl_out <= 3'b001;
+            opcode_sub: begin // sub
+                aluControl_out <= aluControl_sub;
                 doJump_out <= 0;
                 regWriteEnable_out <= 1;
                 memWriteEnable_out <= 0;
-                aluSrcASelect_out <= 0;
+                aluSrcASelect_out <= aluSrcASelect_reg;
                 offsetLayout_out <= offsetLayout_normal;
-                resultSelect_out <= 0;
-                immExtendMode_out <= 0;
+                resultSelect_out <= resultSelect_alu;
+                immExtendMode_out <= immExtendMode_unused;
                 end
-            5'b00010: begin // and
-                aluControl_out <= 3'b010;
+            opcode_and: begin // and
+                aluControl_out <= aluControl_and;
                 doJump_out <= 0;
                 regWriteEnable_out <= 1;
                 memWriteEnable_out <= 0;
-                aluSrcASelect_out <= 0;
+                aluSrcASelect_out <= aluSrcASelect_reg;
                 offsetLayout_out <= offsetLayout_normal;
-                resultSelect_out <= 0;
-                immExtendMode_out <= 0;
+                resultSelect_out <= resultSelect_alu;
+                immExtendMode_out <= immExtendMode_unused;
                 end
-            5'b00011: begin // or
-                aluControl_out <= 3'b011;
+            opcode_or: begin // or
+                aluControl_out <= aluControl_or;
                 doJump_out <= 0;
                 regWriteEnable_out <= 1;
                 memWriteEnable_out <= 0;
-                aluSrcASelect_out <= 0;
+                aluSrcASelect_out <= aluSrcASelect_reg;
                 offsetLayout_out <= offsetLayout_normal;
-                resultSelect_out <= 0;
-                immExtendMode_out <= 0;
+                resultSelect_out <= resultSelect_alu;
+                immExtendMode_out <= immExtendMode_unused;
                 end
-            5'b00100: begin // xor
-                aluControl_out <= 3'b100;
+            opcode_xor: begin // xor
+                aluControl_out <= aluControl_xor;
                 doJump_out <= 0;
                 regWriteEnable_out <= 1;
                 memWriteEnable_out <= 0;
-                aluSrcASelect_out <= 0;
+                aluSrcASelect_out <= aluSrcASelect_reg;
                 offsetLayout_out <= offsetLayout_normal;
-                resultSelect_out <= 0;
-                immExtendMode_out <= 0;
+                resultSelect_out <= resultSelect_alu;
+                immExtendMode_out <= immExtendMode_unused;
                 end
-            5'b00101: begin // jxx
-                aluControl_out <= 3'b000;
+            opcode_jxx: begin // jxx
+                aluControl_out <= aluControl_add;
                 case (jmpCond_in)
                     4'b0000: doJump_out <= isZero_in; // zero
                     4'b0001: doJump_out <= ~isZero_in; // not zero
@@ -88,80 +123,80 @@ module Controller(
                 endcase
                 regWriteEnable_out <= 0;
                 memWriteEnable_out <= 0;
-                aluSrcASelect_out <= 1;
+                aluSrcASelect_out <= aluSrcASelect_zero;
                 offsetLayout_out <= offsetLayout_short;
-                resultSelect_out <= 0;
-                immExtendMode_out <= 0;
+                resultSelect_out <= resultSelect_alu;
+                immExtendMode_out <= immExtendMode_unused;
                 end
-            5'b00110: begin // st
-                aluControl_out <= 3'bzzz;
+            opcode_st: begin // st
+                aluControl_out <= aluControl_unused;
                 doJump_out <= 0;
                 regWriteEnable_out <= 0;
                 memWriteEnable_out <= 1;
-                aluSrcASelect_out <= 1'bz;
+                aluSrcASelect_out <= aluSrcASelect_unused;
                 offsetLayout_out <= offsetLayout_longSplit;
-                resultSelect_out <= 0;
-                immExtendMode_out <= 0;
+                resultSelect_out <= resultSelect_alu;
+                immExtendMode_out <= immExtendMode_unused;
                 end
-            5'b00111: begin // ld
-                aluControl_out <= 3'bzzz;
+            opcode_ld: begin // ld
+                aluControl_out <= aluControl_unused;
                 doJump_out <= 0;
                 regWriteEnable_out <= 1;
                 memWriteEnable_out <= 0;
-                aluSrcASelect_out <= 1'bz;
+                aluSrcASelect_out <= aluSrcASelect_unused;
                 offsetLayout_out <= offsetLayout_longContinuous;
-                resultSelect_out <= 2'b01;
-                immExtendMode_out <= 0;
+                resultSelect_out <= resultSelect_mem;
+                immExtendMode_out <= immExtendMode_unused;
                 end
-            5'b01000: begin // lui
-                aluControl_out <= 3'bzzz;
+            opcode_lui: begin // lui
+                aluControl_out <= aluControl_unused;
                 doJump_out <= 0;
                 regWriteEnable_out <= 1;
                 memWriteEnable_out <= 0;
-                aluSrcASelect_out <= 1'bz;
-                offsetLayout_out <= 2'bzz;
-                resultSelect_out <= 2'b10;
-                immExtendMode_out <= 2'b01;
+                aluSrcASelect_out <= aluSrcASelect_unused;
+                offsetLayout_out <= offsetLayout_unused;
+                resultSelect_out <= resultSelect_imm;
+                immExtendMode_out <= immExtendMode_upper;
                 end
-            5'b01001: begin // lli
-                aluControl_out <= 3'bzzz;
+            opcode_lli: begin // lli
+                aluControl_out <= aluControl_unused;
                 doJump_out <= 0;
                 regWriteEnable_out <= 1;
                 memWriteEnable_out <= 0;
-                aluSrcASelect_out <= 1'bz;
-                offsetLayout_out <= 2'bzz;
-                resultSelect_out <= 2'b10;
-                immExtendMode_out <= 2'b10;
+                aluSrcASelect_out <= aluSrcASelect_unused;
+                offsetLayout_out <= offsetLayout_unused;
+                resultSelect_out <= resultSelect_imm;
+                immExtendMode_out <= immExtendMode_lower;
                 end
-            5'b01010: begin // li
-                aluControl_out <= 3'bzzz;
+            opcode_li: begin // li
+                aluControl_out <= aluControl_unused;
                 doJump_out <= 0;
                 regWriteEnable_out <= 1;
                 memWriteEnable_out <= 0;
-                aluSrcASelect_out <= 1'bz;
-                offsetLayout_out <= 2'bzz;
-                resultSelect_out <= 2'b10;
-                immExtendMode_out <= 2'b11;
+                aluSrcASelect_out <= aluSrcASelect_unused;
+                offsetLayout_out <= offsetLayout_unused;
+                resultSelect_out <= resultSelect_imm;
+                immExtendMode_out <= immExtendMode_full;
                 end
-            5'b01011: begin // int
-                aluControl_out <= 3'bzzz;
+            opcode_int: begin // int
+                aluControl_out <= aluControl_unused;
                 doJump_out <= 0;
                 regWriteEnable_out <= 1'bz;
                 memWriteEnable_out <= 1'bz;
-                aluSrcASelect_out <= 1'bz;
-                offsetLayout_out <= 2'bzz;
-                resultSelect_out <= 1'bz;
-                immExtendMode_out <= 2'bzz;
+                aluSrcASelect_out <= aluSrcASelect_unused;
+                offsetLayout_out <= offsetLayout_unused;
+                resultSelect_out <= resultSelect_unused;
+                immExtendMode_out <= immExtendMode_unused;
                 end
             default: begin
-                aluControl_out <= 3'bzzz;
+                aluControl_out <= aluControl_unused;
                 doJump_out <= 1'bz;
                 regWriteEnable_out <= 1'bz;
                 memWriteEnable_out <= 1'bz;
-                aluSrcASelect_out <= 1'bz;
-                offsetLayout_out <= 2'bzz;
-                resultSelect_out <= 1'bz;
-                immExtendMode_out <= 2'bzz;
+                aluSrcASelect_out <= aluSrcASelect_unused;
+                offsetLayout_out <= offsetLayout_unused;
+                resultSelect_out <= resultSelect_unused;
+                immExtendMode_out <= immExtendMode_unused;
                 end
         endcase
         
