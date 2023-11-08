02d24baac4ad647a68cb4d52f722529cb1744058
TecStylos <tecstylos@gmail.com>
2023-11-08 22:04:59 +0100

    !! Currently in a non working state! !! Use last commit to build a working version. Prepared Operator Sizes

--- QIPU.srcs/sources_1/new/Instruction_Decoder.v
+++ QIPU.srcs/sources_1/new/Instruction_Decoder.v
@@ -1,40 +1,80 @@
 `timescale 1ns / 1ps
 
+
+`define INS_OPCODE_OFFSET                 0
+`define INS_OPCODE_WIDTH                  5
+
+`define INS_REG_DEST_OFFSET               5
+`define INS_REG_DEST_WIDTH                4
+
+`define INS_JMP_COND_OFFSET               5
+`define INS_JMP_COND_WIDTH                4
+
+`define INS_OP_SIZE_OFFSET                9
+`define INS_OP_SIZE_WIDTH                 2
+
+`define INS_OFF_SEL_OFFSET               11
+`define INS_OFF_SEL_WIDTH                 1
+
+`define INS_REG_SRC1_OFFSET              12
+`define INS_REG_SRC1_WIDTH                4
+
+`define INS_REG_SRC2_OFFSET              16
+`define INS_REG_SRC2_WIDTH                4
+
+`define INS_IMM_LONG_OFFSET               9
+`define INS_IMM_LONG_WIDTH               23
+
+`define INS_IMM_SHORT_OFFSET             16
+`define INS_IMM_SHORT_WIDTH              16
+
+`define INS_IMM_SHORT_UNUSED_OFFSET       9
+`define INS_IMM_SHORT_UNUSED_WIDTH        3
+
+`define INS_OFF_SHORT_OFFSET             20
+`define INS_OFF_SHORT_WIDTH              12
+
+`define INS_OFF_LONG_CONT_OFFSET         16
+`define INS_OFF_LONG_CONT_WIDTH          16
+
+`define INS_OFF_LONG_SPLIT_LWR_OFFSET     5
+`define INS_OFF_LONG_SPLIT_LWR_WIDTH      4
+
+`define INS_OFF_LONG_SPLIT_UPR_OFFSET    20
+`define INS_OFF_LONG_SPLIT_UPR_WIDTH     12
+
 module Instruction_Decoder(
         input         clk_i,
         input         rst_i,
         input         enable_i,
         input  [31:0] instr_i,
         
-        output [ 4:0] opcode_o,
-        output [ 3:0] reg_dest_o,
-        output [ 3:0] jmp_cond_o,
-        output        off_sel_o,
-        output [ 3:0] reg_src1_o,
-        output [ 3:0] reg_src2_o,
-        output [22:0] imm_o,
-        output [13:0] off_normal_o,
-        output [12:0] off_short_o,
-        output [17:0] off_long_cont_o,
-        output [17:0] off_long_split_o,
-        output        rel_jmp_o
+        output [(INS_OPCODE_WIDTH        - 1) : 0] opcode_o,
+        output [(INS_REG_DEST_WIDTH      - 1) : 0] reg_dest_o,
+        output [(INS_JMP_COND_WIDTH      - 1) : 0] jmp_cond_o,
+        output [(INS_OP_SIZE_WIDTH       - 1) : 0] op_size_o,
+        output                                     off_sel_o,
+        output [(INS_REG_SRC1_WIDTH      - 1) : 0] reg_src1_o,
+        output [(INS_REG_SRC2_WIDTH      - 1) : 0] reg_src2_o,
+        output [(INS_IMM_MAX_WIDTH       - 1) : 0] imm_o,
+        output [(INS_OFF_SHORT_WIDTH     - 1) : 0] off_short_o,
+        output [(INS_OFF_LONG_CONT_WIDTH - 1) : 0] off_long_cont_o,
+        output [(INS_OFF_LONG_CONT_WIDTH - 1) : 0] off_long_split_o
     );
     
     reg  [31:0] instr;
-    wire [31:0] temp_instr = instr; //enable_i ? instr_i : instr;
     
-    assign opcode_o         = temp_instr[ 4: 0];
-    assign reg_dest_o       = temp_instr[ 8: 5];
-    assign jmp_cond_o       = temp_instr[ 8: 5];
-    assign off_sel_o        = temp_instr[    9];
-    assign reg_src1_o       = temp_instr[13:10];
-    assign reg_src2_o       = temp_instr[17:14];
-    assign imm_o            = temp_instr[31: 9];
-    assign off_normal_o     = temp_instr[31:18];
-    assign off_short_o      = temp_instr[30:18];
-    assign off_long_cont_o  = temp_instr[31:14];
-    assign off_long_split_o ={temp_instr[31:18], temp_instr[8:5] };
-    assign rel_jmp_o        = temp_instr[   31];
+    assign opcode_o         = instr[(INS_OPCODE_OFFSET             + INS_OPCODE_WIDTH             - 1) : INS_OPCODE_OFFSET            ];
+    assign reg_dest_o       = instr[(INS_REG_DEST_OFFSET           + INS_REG_DEST_WIDTH           - 1) : INS_REG_DEST_OFFSET          ];
+    assign jmp_cond_o       = instr[(INS_JMP_COND_OFFSET           + INS_JMP_COND_WIDTH           - 1) : INS_JMP_COND_OFFSET          ];
+    assign off_sel_o        = instr[(INS_OFF_SEL_OFFSET            + INS_OFF_SEL_WIDTH            - 1) : INS_OFF_SEL_OFFSET           ];
+    assign reg_src1_o       = instr[(INS_REG_SRC1_OFFSET           + INS_REG_SRC1_WIDTH           - 1) : INS_REG_SRC1_OFFSET          ];
+    assign reg_src2_o       = instr[(INS_REG_SRC2_OFFSET           + INS_REG_SRC2_WIDTH           - 1) : INS_REG_SRC2_OFFSET          ];
+    assign imm_o            = instr[(INS_IMM_LONG_OFFSET           + INS_IMM_LONG_WIDTH           - 1) : INS_IMM_LONG_OFFSET          ];
+    assign off_short_o      = instr[(INS_OFF_SHORT_OFFSET          + INS_OFF_SHORT_WIDTH          - 1) : INS_OFF_SHORT_OFFSET         ];
+    assign off_long_cont_o  = instr[(INS_OFF_LONG_CONT_OFFSET      + INS_OFF_LONG_CONT_WIDTH      - 1) : INS_OFF_LONG_CONT_OFFSET     ];
+    assign off_long_split_o ={instr[(INS_OFF_LONG_SPLIT_UPR_OFFSET + INS_OFF_LONG_SPLIT_UPR_WIDTH - 1) : INS_OFF_LONG_SPLIT_UPR_OFFSET],
+                              instr[(INS_OFF_LONG_SPLIT_LWR_OFFSET + INS_OFF_LONG_SPLIT_LWR_WIDTH - 1) : INS_OFF_LONG_SPLIT_LWR_OFFSET]};
     
     always @ (posedge clk_i) begin
         if (rst_i)
